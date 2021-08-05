16a9cb9c6dbbf06cbcf4d83b9a04edd1ae5eb543
jerrykankelborg <jkankelborg@csumb.edu>
2021-08-04 22:51:33 -0700

    added search bar with transition effect

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener("click", () => container.classList.add("show-nav"));
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener("click", () => container.classList.remove("show-nav"));
+
+btn.addEventListener("click", () => {
+  search.classList.add("active");
+  input.focus();
+});
